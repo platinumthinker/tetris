330b95010fc25c18c4869abd6472409d931edadf
Andrey Teplyashin <andrey.teplyashin@travelping.com>
2018-03-18 12:25:28 +0700

    Init commit

---
+++ src/main/java/BasicGameApp.java
@@ -0,0 +1,16 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+public class BasicGameApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
